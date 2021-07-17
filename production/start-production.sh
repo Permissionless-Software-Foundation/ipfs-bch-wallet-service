6f64fc4652d614f8c76f7c9b11c45129823082be
Chris Troutner <chris.troutner@gmail.com>
2021-07-17 11:16:57 -0700

    fix(production): Adjusting production settings 2

--- production/start-production.sh
+++ production/start-production.sh
@@ -11,6 +11,9 @@ export PORT=5001 # REST API
 export IPFS_TCP_PORT=5668 # IPSF TCP Port
 export IPFS_WS_PORT=5669 # IPFS WS Port
 
+# Production database connection string.
+export DBURL=mongodb://172.17.0.1:5555/ipfs-service-prod
+
 #export ENABLE_CIRCUIT_RELAY=1
 
 export SVC_ENV=production
