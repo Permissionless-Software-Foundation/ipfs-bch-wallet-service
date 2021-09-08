29883747696f663be5ea9b9e52792ce3821fdb36
Chris Troutner <chris.troutner@gmail.com>
2021-09-08 12:35:26 -0700

    linting

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -7,55 +7,55 @@
 // Public npm libraries.
 
 // Load the Clean Architecture Adapters library
-const Adapters = require("../adapters");
+const Adapters = require('../adapters')
 
 // Load the JSON RPC Controller.
-const JSONRPC = require("./json-rpc");
+const JSONRPC = require('./json-rpc')
 
 // Load the Clean Architecture Use Case libraries.
-const UseCases = require("../use-cases");
+const UseCases = require('../use-cases')
 // const useCases = new UseCases({ adapters })
 
 // Load the REST API Controllers.
-const RESTControllers = require("./rest-api");
+const RESTControllers = require('./rest-api')
 
 class Controllers {
-  constructor(localConfig = {}) {
-    this.adapters = new Adapters();
-    this.useCases = new UseCases({ adapters: this.adapters });
+  constructor (localConfig = {}) {
+    this.adapters = new Adapters()
+    this.useCases = new UseCases({ adapters: this.adapters })
   }
 
-  async attachControllers(app) {
+  async attachControllers (app) {
     // Wait for any startup processes to complete for the Adapters libraries.
-    await this.adapters.start();
+    await this.adapters.start()
 
-    this.attachRPCControllers();
+    this.attachRPCControllers()
   }
 
   // Top-level function for this library.
   // Start the various Controllers and attach them to the app.
-  attachRESTControllers(app) {
+  attachRESTControllers (app) {
     const restControllers = new RESTControllers({
       adapters: this.adapters,
-      useCases: this.useCases,
-    });
+      useCases: this.useCases
+    })
 
     // Attach the REST API Controllers associated with the boilerplate code to the Koa app.
-    restControllers.attachRESTControllers(app);
+    restControllers.attachRESTControllers(app)
   }
 
   // Add the JSON RPC router to the ipfs-coord adapter.
-  attachRPCControllers() {
+  attachRPCControllers () {
     const jsonRpcController = new JSONRPC({
       adapters: this.adapters,
-      useCases: this.useCases,
-    });
+      useCases: this.useCases
+    })
 
     // Attach the input of the JSON RPC router to the output of ipfs-coord.
     this.adapters.ipfs.ipfsCoordAdapter.attachRPCRouter(
       jsonRpcController.router
-    );
+    )
   }
 }
 
-module.exports = Controllers;
+module.exports = Controllers
--- test/unit/controllers/controllers.unit.js
+++ test/unit/controllers/controllers.unit.js
@@ -3,61 +3,61 @@
 */
 
 // Public npm libraries
-const assert = require("chai").assert;
-const sinon = require("sinon");
+const assert = require('chai').assert
+const sinon = require('sinon')
 
-const Controllers = require("../../../src/controllers");
+const Controllers = require('../../../src/controllers')
 
-describe("#Controllers", () => {
-  let uut;
-  let sandbox;
+describe('#Controllers', () => {
+  let uut
+  let sandbox
 
   beforeEach(() => {
-    sandbox = sinon.createSandbox();
+    sandbox = sinon.createSandbox()
 
-    uut = new Controllers();
-  });
+    uut = new Controllers()
+  })
 
-  afterEach(() => sandbox.restore());
+  afterEach(() => sandbox.restore())
 
-  describe("#attachControllers", () => {
-    it("should attach the controllers", async () => {
+  describe('#attachControllers', () => {
+    it('should attach the controllers', async () => {
       // mock dependencies
       // sandbox.stub(adapters.fullStackJwt, 'getJWT').resolves({})
       // sandbox.stub(adapters.fullStackJwt, 'instanceBchjs').resolves({})
       // sandbox.stub(adapters.ipfs, 'start').resolves({})
       // adapters.ipfs.ipfsCoordAdapter = {
 
-      sandbox.stub(uut.adapters, "start").resolves({});
+      sandbox.stub(uut.adapters, 'start').resolves({})
       uut.adapters.ipfs.ipfsCoordAdapter = {
-        attachRPCRouter: () => {},
-      };
+        attachRPCRouter: () => {}
+      }
 
       const app = {
-        use: () => {},
-      };
+        use: () => {}
+      }
 
-      await uut.attachControllers(app);
+      await uut.attachControllers(app)
 
-      assert.isOk(true, "Not throwing an error is a success");
-    });
+      assert.isOk(true, 'Not throwing an error is a success')
+    })
 
-    it("should catch and throw errors", async () => {
+    it('should catch and throw errors', async () => {
       try {
         // Force an error
-        sandbox.stub(uut.adapters, "start").rejects(new Error("test error"));
+        sandbox.stub(uut.adapters, 'start').rejects(new Error('test error'))
 
         const app = {
-          use: () => {},
-        };
+          use: () => {}
+        }
 
-        await uut.attachControllers(app);
+        await uut.attachControllers(app)
 
-        assert.fail("Unexpected code path");
+        assert.fail('Unexpected code path')
       } catch (err) {
         // console.log('err.message: ', err.message)
-        assert.include(err.message, "test error");
+        assert.include(err.message, 'test error')
       }
-    });
-  });
-});
+    })
+  })
+})
